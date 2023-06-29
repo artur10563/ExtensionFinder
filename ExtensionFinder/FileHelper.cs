3f1c291c6dfe3910a191b82048f383adbe4058ec
artur10563 <galkaartur2004galkaartur@gmail.com>
2023-06-29 23:16:28 +0300

    BACKUP

---
+++ ExtensionFinder/FileHelper.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace ExtensionFinder
+{
+    public static class FileHelper
+    {
+        private static char mirror_malware = '\u202e';
+        public static List<string> GetFiles(string directory)
+        {
+            List<string> files = new List<string>();
+            try
+            {
+                foreach (string file in Directory.GetFiles(directory))
+                {
+
+                    files.Add(file.Replace(directory + @"\", ""));
+                }
+            }
+            //На випадок якшо папка вимагає адмінського доступу
+            catch { }
+
+
+            return files;
+        }
+
+        public static List<string> GetDirs(string directory)
+        {
+            List<string> dirs = new List<string>();
+
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(directory))
+                {
+                    dirs.Add(dir);
+                }
+            }
+            catch { }
+            return dirs;
+        }
+
+        //Main logic
+        private static void ScanDirHelper(string directory,
+            ref int scanedFiles, ref int scanedDirs, ref int foundFiles, ref int foundDirs)
+        {
+
+            List<string> dirs = GetDirs(directory);
+
+            foreach (string dir in dirs)
+            {
+                scanedDirs++;
+
+                List<string> files = GetFiles(dir);
+
+                //Check if folder contains any mirrored files or has keyword files
+                bool hasKeyWordFiles = files.Any(file => file.Contains(mirror_malware));
+
+
+                if (hasKeyWordFiles)
+                {
+
+                    foundDirs++;
+                    Console.Write("\n" + dir + "\n");
+
+                    foreach (string file in files)
+                    {
+                        scanedFiles++;
+
+                        if (file.Contains(mirror_malware))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            foundFiles++;
+                            Console.WriteLine("\t" + file);
+                        }
+                        Console.ResetColor();
+
+                    }
+                }
+
+                if (GetDirs(dir).Count > 0)
+                    ScanDirHelper(dir, ref scanedFiles, ref scanedDirs, ref foundFiles, ref foundDirs);
+            }
+
+        }
+
+        public static void ScanDir(string directory)
+        {
+            if (!Directory.Exists(directory)) return;
+
+            int scanedFiles = 0;
+            int scanedDirs = 0;
+
+            int foundFiles = 0;
+            int foundDirs = 0;
+
+            scanedDirs++;
+
+            List<string> files = GetFiles(directory);
+
+            bool hasKeyWordFiles = files.Any(file => file.Contains(mirror_malware));
+
+            if (hasKeyWordFiles)
+            {
+                foundDirs++;
+                Console.Write("\n" + directory + "\n");
+
+                foreach (string file in files)
+                {
+                    scanedFiles++;
+
+                    if (file.Contains(mirror_malware))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        foundFiles++;
+                        Console.WriteLine("\t" + file);
+                    }
+                    Console.ResetColor();
+                }
+            }
+
+
+            //Scan all other folders
+            ScanDirHelper(directory, ref scanedFiles, ref scanedDirs, ref foundFiles, ref foundDirs);
+            Console.Write(
+             "\n\nScaned dirs : " + scanedDirs +
+               "\nScaned files: " + scanedFiles +
+               "\n\nDirs with malware files : " + foundDirs +
+               "\nMalware files: " + foundFiles);
+
+
+        }
+    }
+}
---
+++ ExtensionFinder/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ExtensionFinder
+{
+    internal class Program
+    {
+        private static void ShowHelp()
+        {
+            Console.WriteLine("Usage: ExtensionFinder [path]\n");
+            Console.WriteLine("Arguments:");
+            Console.WriteLine("  [path]  Path from which scan is started (e.g., C:/ , C:\\Folder1\\Folder2\\Folder3)");
+        }
+        static void Main(string[] args)
+        {
+            ;
+
+            //No args - Help
+            if (args.Length == 0)
+            {
+                ShowHelp();
+            }
+
+            //One argument
+            else if (args.Length == 1)
+            {
+                //Help
+                if (args[0] == "--help")
+                {
+                    ShowHelp();
+                }
+                //Check if such path exists
+                else
+                {
+                    string path = args[0];
+                    if (Directory.Exists(path))
+                    {
+                        var start = DateTime.Now;
+                        Console.WriteLine("Scan started from " + path);
+                        Console.WriteLine("It might take some time!\n");
+
+                        FileHelper.ScanDir(path);
+
+                        Console.WriteLine("\nScan finished!");
+                        var duration = DateTime.Now - start;
+                        StringBuilder sb = new StringBuilder();
+                        //через стрінг білдер динамічно робити шоб добавляти тільки якшо більше нуля
+                        string formattedDuration = string.Format("{0}d{1}h{2}m{3}s{4}ms",
+                            duration.Days, duration.Hours, duration.Minutes, duration.Seconds, duration.Milliseconds);
+
+                        Console.WriteLine("\nScan took " + formattedDuration);
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No such directory as " + path);
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
